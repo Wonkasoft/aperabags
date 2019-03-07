be10399155a8646f36b351f78cdbc63968d88241
MrLister <m_r_lister@yahoo.com>
2019-03-07 00:26:47 -0800

    established js file

---
+++ js/wonkamizer-js.js
@@ -0,0 +1,32 @@
+( function($)
+{
+	"use strict";
+
+	/*===================================================================
+	=            This is area for writing callable functions            =
+	===================================================================*/
+	function first_function()
+	{
+
+	}	
+	/*=====  End of This is area for writing callable functions  ======*/
+
+	/*====================================================================
+	=            This is for loading calls on window resizing            =
+	====================================================================*/
+	window.onresize = function()
+	{
+
+	};
+	/*=====  End of This is for loading calls on window resizing  ======*/
+	
+	/*===================================================================
+	=            This is for running after document is ready            =
+	===================================================================*/
+	window.onload = function()
+	{
+
+	};
+	/*=====  End of This is for running after document is ready  ======*/
+	
+})(jQuery);
