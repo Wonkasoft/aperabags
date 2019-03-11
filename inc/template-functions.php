922a2c475bd55dc5d07b91e69c7ffa97c21891bf
MrLister <m_r_lister@yahoo.com>
2019-03-11 10:26:34 -0700

    fixing menus

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -220,3 +220,14 @@ function the_mods_for_section( $section ) {
 	return false;
 }
 add_action( 'get_mods_before_section', 'the_mods_for_section', 10, 1 );
+
+/**
+ * customize gravity forms
+ */
+function add_bootstrap_container_class( $field_container, $field, $form, $css_class, $style, $field_content ) {
+	$id = $field->id;
+	$field_id = is_admin() || empty( $form ) ? "field_{$id}" : 'field_' . $form['id'] . "_$id";
+	
+	return '<li id="' . $field_id . '" class="' . $css_class . ' form-group">{FIELD_CONTENT}</li>';
+}
+add_filter( 'gform_field_container', 'add_bootstrap_container_class', 10, 6 );
