7f5761bf05d9a8635609ed7f6ec4fefdb2ba6101
llister15 <llister@wonkasoft.com>
2019-03-12 01:37:15 -0700

    updated wocommerce.php

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -33,15 +33,15 @@ function apera_bags_woocommerce_scripts() {
 
 	$font_path   = WC()->plugin_url() . '/assets/fonts/';
 	$inline_font = '@font-face {
-			font-family: "star";
-			src: url("' . $font_path . 'star.eot");
-			src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
-				url("' . $font_path . 'star.woff") format("woff"),
-				url("' . $font_path . 'star.ttf") format("truetype"),
-				url("' . $font_path . 'star.svg#star") format("svg");
-			font-weight: normal;
-			font-style: normal;
-		}';
+		font-family: "star";
+		src: url("' . $font_path . 'star.eot");
+		src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
+		url("' . $font_path . 'star.woff") format("woff"),
+		url("' . $font_path . 'star.ttf") format("truetype"),
+		url("' . $font_path . 'star.svg#star") format("svg");
+		font-weight: normal;
+		font-style: normal;
+	}';
 
 	wp_add_inline_style( 'apera-bags-woocommerce-style', $inline_font );
 }
@@ -161,12 +161,12 @@ if ( ! function_exists( 'apera_bags_woocommerce_wrapper_before' ) ) {
 		?>
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-			<?php
-	}
-}
-add_action( 'woocommerce_before_main_content', 'apera_bags_woocommerce_wrapper_before' );
+				<?php
+			}
+		}
+		add_action( 'woocommerce_before_main_content', 'apera_bags_woocommerce_wrapper_before' );
 
-if ( ! function_exists( 'apera_bags_woocommerce_wrapper_after' ) ) {
+		if ( ! function_exists( 'apera_bags_woocommerce_wrapper_after' ) ) {
 	/**
 	 * After Content.
 	 *
@@ -175,11 +175,11 @@ if ( ! function_exists( 'apera_bags_woocommerce_wrapper_after' ) ) {
 	 * @return void
 	 */
 	function apera_bags_woocommerce_wrapper_after() {
-			?>
-			</main><!-- #main -->
-		</div><!-- #primary -->
-		<?php
-	}
+		?>
+	</main><!-- #main -->
+</div><!-- #primary -->
+<?php
+}
 }
 add_action( 'woocommerce_after_main_content', 'apera_bags_woocommerce_wrapper_after' );
 
@@ -195,7 +195,7 @@ add_action( 'woocommerce_after_main_content', 'apera_bags_woocommerce_wrapper_af
 	?>
  */
 
-if ( ! function_exists( 'apera_bags_woocommerce_cart_link_fragment' ) ) {
+	if ( ! function_exists( 'apera_bags_woocommerce_cart_link_fragment' ) ) {
 	/**
 	 * Cart Fragments.
 	 *
@@ -240,12 +240,90 @@ if ( ! function_exists( 'apera_bags_woocommerce_cart_link' ) ) {
 
 // define the woocommerce_product_thumbnails callback 
 function action_woocommerce_product_thumbnails() { 
-    global $products;
-    echo "<pre>\n";
-    print_r( $products );
-    echo "</pre>\n";
+	global $products;
+	echo "<pre>\n";
+	print_r( $products );
+	echo "</pre>\n";
     // _e( '<img src="" />' );
 }; 
-         
+
 // add the action 
 add_action( 'woocommerce_template_loop_product_thumbnail', 'action_woocommerce_product_thumbnails', 10 ); 
+
+	/**
+	 * Image Flipper class
+	 */
+if ( ! class_exists( 'WC_pif' ) ) {
+	class WC_pif {
+		public function __construct() {
+			add_action( 'init', array( $this, 'pif_init' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'pif_scripts' ) );
+			add_action( 'woocommerce_before_shop_loop_item_title', array( $this, 'woocommerce_template_loop_second_product_thumbnail' ), 11 );
+			add_filter( 'post_class', array( $this, 'product_has_gallery' ) );
+		}
+			/**
+			 * Plugin initilisation
+			 */
+			public function pif_init() {
+				load_plugin_textdomain( 'woocommerce-product-image-flipper', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+			}
+			/**
+			 * Class functions
+			 */
+			public function pif_scripts() {
+				if ( apply_filters( 'woocommerce_product_image_flipper_styles', true ) ) {
+					wp_enqueue_style( 'pif-styles', plugins_url( '/assets/css/style.css', __FILE__ ) );
+				}
+			}
+			public function product_has_gallery( $classes ) {
+				global $product;
+				$post_type = get_post_type( get_the_ID() );
+				if ( ! is_admin() ) {
+					if ( $post_type == 'product' ) {
+						$attachment_ids = $this->get_gallery_image_ids( $product );
+						if ( $attachment_ids ) {
+							$classes[] = 'pif-has-gallery';
+						}
+					}
+				}
+				return $classes;
+			}
+			/**
+			 * Frontend functions
+			 */
+			public function woocommerce_template_loop_second_product_thumbnail() {
+				global $product, $woocommerce;
+				$attachment_ids = $this->get_gallery_image_ids( $product );
+				if ( $attachment_ids ) {
+					$attachment_ids     = array_values( $attachment_ids );
+					$secondary_image_id = $attachment_ids['0'];
+					$secondary_image_alt = get_post_meta( $secondary_image_id, '_wp_attachment_image_alt', true );
+					$secondary_image_title = get_the_title($secondary_image_id);
+					echo wp_get_attachment_image(
+						$secondary_image_id,
+						'shop_catalog',
+						'',
+						array(
+							'class' => 'secondary-image attachment-shop-catalog wp-post-image wp-post-image--secondary',
+							'alt' => $secondary_image_alt,
+							'title' => $secondary_image_title
+						)
+					);
+				}
+			}
+			/**
+			 * WooCommerce Compatibility Functions
+			 */
+			public function get_gallery_image_ids( $product ) {
+				if ( ! is_a( $product, 'WC_Product' ) ) {
+					return;
+				}
+				if ( is_callable( 'WC_Product::get_gallery_image_ids' ) ) {
+					return $product->get_gallery_image_ids();
+				} else {
+					return $product->get_gallery_attachment_ids();
+				}
+			}
+		}
+		$WC_pif = new WC_pif();
+	}
