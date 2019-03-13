c39df8af655aad363da1b469c96a5dec4808a717
MrLister <m_r_lister@yahoo.com>
2019-03-13 01:13:45 -0700

    getting close

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -238,86 +238,16 @@ if ( ! function_exists( 'apera_bags_woocommerce_cart_link' ) ) {
 	}
 }
 
-/**
- * Image Flipper class
- */
-if ( ! class_exists( 'WC_pif' ) ) {
-	class WC_pif 
-	{
-		public function __construct() {
-			add_action( 'init', array( $this, 'pif_init' ) );
-			add_action( 'woocommerce_before_shop_loop_item_title', array( $this, 'woocommerce_template_loop_second_product_thumbnail' ), 11 );
-			add_filter( 'post_class', array( $this, 'product_has_gallery' ) );
-		}
-		/**
-		 * Plugin initilisation
-		 */
-		public function pif_init() {
-			load_plugin_textdomain( 'woocommerce-product-image-flipper', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-		}
-		
-		public function product_has_gallery( $classes ) {
-			global $product;
-			$post_type = get_post_type( get_the_ID() );
-			if ( ! is_admin() ) {
-				if ( $post_type == 'product' ) {
-					$attachment_ids = $this->get_gallery_image_ids( $product );
-					if ( $attachment_ids ) {
-						$classes[] = 'pif-has-gallery';
-					}
-				}
-			}
-			return $classes;
-		}
-		/**
-		 * Frontend functions
-		 */
-		public function woocommerce_template_loop_second_product_thumbnail() {
-			global $product, $woocommerce;
-			$attachment_ids = $this->get_gallery_image_ids( $product );
-			if ( $attachment_ids ) {
-				$attachment_ids     = array_values( $attachment_ids );
-				$secondary_image_id = $attachment_ids['0'];
-				$secondary_image_alt = get_post_meta( $secondary_image_id, '_wp_attachment_image_alt', true );
-				$secondary_image_title = get_the_title($secondary_image_id);
-				echo wp_get_attachment_image(
-					$secondary_image_id,
-					'shop_catalog',
-					'',
-					array(
-						'class' => 'secondary-image attachment-shop-catalog wp-post-image wp-post-image--secondary',
-						'alt' => $secondary_image_alt,
-						'title' => $secondary_image_title
-					)
-				);
-			}
-		}
-		/**
-		 * WooCommerce Compatibility Functions
-		 */
-		public function get_gallery_image_ids( $product ) {
-			if ( ! is_a( $product, 'WC_Product' ) ) {
-				return;
-			}
-			if ( is_callable( 'WC_Product::get_gallery_image_ids' ) ) {
-				return $product->get_gallery_image_ids();
-			} else {
-				return $product->get_gallery_attachment_ids();
-			}
-		}
-	}
-
-	
-	/* This is the init for this class */
-	// $WC_pif = new WC_pif();
-}
 
 /**
  * This function is to override the parsing of the images during a shop loop
  * 
  */
 function wonka_customized_shop_loop() {
-	global $product, $woocommerce;
+	/*========================================================
+	=            For setting up the image flipper            =
+	========================================================*/
+	global $product;
 
 	if ( ! is_a( $product, 'WC_Product' ) ) {
 				return;
@@ -334,12 +264,28 @@ function wonka_customized_shop_loop() {
 		$secondary_image_title = get_the_title($secondary_image_id);
 	endif;
 
+	add_filter( 'post_class', function( $classes ) {
+		global $product;
+		$post_type = get_post_type( get_the_ID() );
+			if ( ! is_admin() ) {
+				if ( $post_type == 'product' ) {
+					$attachment_ids = $product->get_gallery_image_ids( $product );
+					if ( $attachment_ids ) {
+						$classes[] = 'pif-has-gallery';
+					}
+				}
+			}
+			return $classes;
+
+	} );
+	/*=====  End of For setting up the image flipper  ======*/
+	
 	$output = '';
 	ob_start();
 	$output .= '<div class="wonka-shop-img-wrap">';
 	$output .= '<img src="' . esc_url( get_the_post_thumbnail_url( get_the_ID(), 'full' ) ) . '" class="img-fluid wonka-img-fluid" />';
 	if ( $attachment_ids ) :
-		$output .= '<img src="' . esc_url( get_the_post_thumbnail_url( $secondary_image_id, 'full' ) ) . '" title="' . $secondary_image_title . '" alt="' . $secondary_image_alt . '" class="secondary-image attachment-shop-catalog wp-post-image wp-post-image--secondary" />';
+		$output .= '<img src="' . esc_url( wp_get_attachment_url( $secondary_image_id ) ) . '" title="' . $secondary_image_title . '" alt="' . $secondary_image_alt . '" class="secondary-image attachment-shop-catalog wp-post-image wp-post-image--secondary" />';
 	endif;
 	$output .= '</div><!-- .wonka-shop-img-wrap -->';
 	ob_end_clean();
