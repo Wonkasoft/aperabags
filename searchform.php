97dc63d847e1d38fe4ffbe2095ebe85078bc87f5
llister15 <llister@wonkasoft.com>
2019-03-13 02:56:46 -0700

    update

--- searchform.php
+++ searchform.php
@@ -10,5 +10,5 @@
   <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label for="s" class="assistive-text sr-only"><?php _e( 'Search', 'apera' ); ?></label>
     <input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'apera' ); ?>" />
-    <input type="submit" class="submit wonka-btn" name="submit" id="searchsubmit" value="<?php esc_attr_e( '<i class="fa fa-search"></i>', 'apera' ); ?>" />
+    <button type="submit" class="submit wonka-btn" name="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
   </form>
