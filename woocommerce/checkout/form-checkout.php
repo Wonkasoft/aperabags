065353729ee9998451ef4c6a0d12ccbbd687d2e5
MrLister <m_r_lister@yahoo.com>
2019-03-14 05:56:15 -0700

    checkout page styles

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -35,7 +35,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-		<div class="row wonka-row" id="customer_details">
+		<div class="wonka-wrap" id="customer_details">
 			<div class="col col-6">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 			</div>
