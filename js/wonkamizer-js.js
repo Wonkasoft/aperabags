2e554db02f67d48bf7e3e1e5152e400b9a7826d5
MrLister <m_r_lister@yahoo.com>
2019-03-14 10:16:57 -0700

    product summary scrolling

--- js/wonkamizer-js.js
+++ js/wonkamizer-js.js
@@ -87,17 +87,12 @@
 		 }
 	}
 
-	var allowed = true;
 	function stickySummary() 
 	{
 		var summary_section = document.querySelector( '.summary.entry-summary' ),
 		img_area = document.querySelector( '.wonka-single-product-img' ),
 		target_scrolling = img_area.offsetHeight - summary_section.offsetHeight,
 		win_y = window.pageYOffset;
-		console.log('scrolling: ' + target_scrolling);
-		console.log('img height: ' + img_area.offsetHeight);
-		console.log('summary top: ' + summary_section.offsetTop);
-		console.log('windowPageY: ' + win_y);
 
 		if ( window.innerWidth > 792 && win_y - summary_section.parentElement.offsetTop > target_scrolling ) 
 		{
