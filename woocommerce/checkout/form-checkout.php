87a6ae0a85ef5fc5926b883ababa6e04e7d548f7
MrLister <m_r_lister@yahoo.com>
2019-03-14 03:31:14 -0700

    checkout page styles

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -44,21 +44,31 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 			</div>
 		</div>
-
-		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+		<?php /* Added woocommerce_checkout_after_customer_details row and col */ ?>
+		
+		<div class="row">
+			<div class="col col-12">
+				<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+			</div><!-- .col-12 -->
+		</div><!-- .row -->
 
 	<?php endif; ?>
 
-	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+	<?php /* Added row and col */ ?>
+	
+	<div class="row">
+		<div class="col col-12">
+			<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+			<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
+			<div id="order_review" class="woocommerce-checkout-review-order">
+				<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+			</div>
 
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
-
+			<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+		</div><!-- .col-12 -->
+	</div><!-- .row -->
 </form>
 
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
