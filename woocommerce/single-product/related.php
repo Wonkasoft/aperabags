fb0156a82306457cb19ed81fb4677ad23901d349
llister15 <llister@wonkasoft.com>
2019-03-14 02:49:08 -0700

    update

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -21,7 +21,7 @@ if ( $related_products ) : ?>
 
   <section class="related products text-center">
 
-    <h4 class="related-product-title"><?php esc_html_e( 'Products you may love...', 'woocommerce' ); ?></h4>
+    <h4 class="related-product-title"><?php esc_html_e( 'Products you may love', 'woocommerce' ); ?></h4>
 
     <?php woocommerce_product_loop_start(); ?>
 
