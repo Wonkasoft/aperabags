012f14dff31e5affabc908f65f6244c8a3c434d0
MrLister <m_r_lister@yahoo.com>
2019-03-18 02:21:58 -0700

    shop button

--- front-page.php
+++ front-page.php
@@ -91,7 +91,7 @@ get_header();
 					</div><!-- .row -->
 					<div class="row">
 						<div class="col col-12 text-center">
-							<a href="/product-category/all-bags/" class="wonka-btn" target="_self"></a>
+							<a href="/product-category/all-bags/" class="wonka-btn" target="_self"><?php _e( __( 'Shop More' ) ) ?></a>
 						</div><!-- .col -->
 					</div><!-- .row -->
 				</div><!-- .col-12 -->
