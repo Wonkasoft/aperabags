a5e95d67bcf1f77e723dc905f28954f6a831b76c
MrLister <m_r_lister@yahoo.com>
2019-03-19 03:43:17 -0700

    additions to cart

--- woocommerce/cart/cart-totals.php
+++ woocommerce/cart/cart-totals.php
@@ -92,24 +92,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php endif; ?>
 
 		<?php if ( wc_coupons_enabled() ) : ?>
-		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-			<div class="panel panel-default">
-				<div class="panel-heading" role="tab" id="headingOne">
-					<h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-							Add Promo Code (Optional)
-						</a>
-					</h4>
+			<tr>
+				<td>
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+						<div class="panel panel-default">
+							<div class="panel-heading" role="tab" id="headingOne">
+								<h4 class="panel-title">
+									<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+										Add Promo Code (Optional)
+									</a>
+								</h4>
 
-				</div>
-				<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-					<div class="panel-body"><div class="coupon form-group form-inline">
-						<label for="coupon_code" class="sr-only"><?php esc_html__( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text form-control" id="coupon_code" value="" placeholder="<?php esc_attr__( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button wonka-btn" name="apply_coupon" value="<?php esc_attr__( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr__( 'Apply coupon', 'woocommerce' ); ?></button>
-						<?php do_action( 'woocommerce_cart_coupon' ); ?>
+							</div>
+							<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+								<div class="panel-body"><div class="coupon form-group form-inline">
+									<label for="coupon_code" class="sr-only"><?php esc_html__( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text form-control" id="coupon_code" value="" placeholder="<?php esc_attr__( 'Coupon code', 'woocommerce' ); ?>" /> <button type="submit" class="button wonka-btn" name="apply_coupon" value="<?php esc_attr__( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr__( 'Apply coupon', 'woocommerce' ); ?></button>
+									<?php do_action( 'woocommerce_cart_coupon' ); ?>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			</td>
+		</tr>
 	<?php endif; ?>
 		<?php do_action( 'woocommerce_cart_totals_before_order_total' ); ?>
 
