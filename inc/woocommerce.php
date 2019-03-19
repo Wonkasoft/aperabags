148a67e3281c946e84fc59499a25ff28181573c3
MrLister <m_r_lister@yahoo.com>
2019-03-19 14:06:30 -0700

    fixing single product page

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -347,6 +347,6 @@ add_action( 'woocommerce_after_cart', 'woocommerce_cross_sell_display' );
 function wonka_add_continue_shopping_notice_to_cart() {
 	$message = sprintf( '<a href="%s" tabindex="1" class="button wc-forward">%s</a>', esc_url( '/shop' ), esc_html__( 'Continue shopping', 'woocommerce' ) );
 
-	wc_add_notice( $message );
+	echo $message;
 }
 add_action( 'woocommerce_before_cart', 'wonka_add_continue_shopping_notice_to_cart', 10 );
