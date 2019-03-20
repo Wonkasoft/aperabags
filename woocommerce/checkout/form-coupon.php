49bf20e5cb1b598942598ad3728f84b1b418548a
MrLister <m_r_lister@yahoo.com>
2019-03-20 00:42:24 -0700

    checkout coupon form

--- woocommerce/checkout/form-coupon.php
+++ woocommerce/checkout/form-coupon.php
@@ -26,15 +26,15 @@ if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', __( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . __( 'Click here to enter your code', 'woocommerce' ) . '</a>' ), 'notice' ); ?>
 </div>
 
-<form class="checkout_coupon woocommerce-form-coupon form-inline" method="post" style="display:none">
+<form class="checkout_coupon woocommerce-form-coupon" method="post" style="display:none">
 
 	<p><?php esc_html_e( 'If you have a coupon code, please apply it below.', 'woocommerce' ); ?></p>
 
-	<p class="form-row form-row-first">
+	<p class="form-row form-row-first form-inline">
 		<input type="text" name="coupon_code" class="input-text form-control" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
 	</p>
 
-	<p class="form-row form-row-last">
+	<p class="form-row form-row-last form-inline">
 		<button type="submit" class="button wonka-btn" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply coupon', 'woocommerce' ); ?></button>
 	</p>
 
