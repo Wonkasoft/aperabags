7f40fd814cd6811cc99a92212db68037a6dc9b91
MrLister <m_r_lister@yahoo.com>
2019-03-25 17:28:32 -0700

    front page

--- front-page.php
+++ front-page.php
@@ -187,12 +187,12 @@ get_header();
 							  <div class="modal-dialog" role="document">
 							    <div class="modal-content">
 							      <div class="modal-body">
-							       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							          <span aria-hidden="true">&times;</span>
-							        </button>        
 							        <!-- 16:9 aspect ratio -->
 									<div class="embed-responsive embed-responsive-16by9">
-									<iframe width="560" height="315" src="https://www.youtube.com/embed/<?php _e( $videocode ); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+									<iframe width="780" height="442" src="https://www.youtube.com/embed/<?php _e( $videocode ); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+								       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								          <span aria-hidden="true">&times;</span>
+								        </button>        
 									</div>
 							      </div>
 							    </div>
