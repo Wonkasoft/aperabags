c88eeaff3a88c95da5701e5aa2fd2f84c5e08c40
llister15 <llister@wonkasoft.com>
2019-04-06 01:59:09 -0700

    update

--- front-page.php
+++ front-page.php
@@ -183,7 +183,7 @@ get_header();
 		if ( ! empty( $cause->cause_modal_video ) ) : ?>
 			
 		<!-- Modal -->
-			<div class="modal fade" id="videoModal2" tabindex="-1" role="dialog" aria-labelledby="causeAperaModal" aria-hidden="true">
+			<div class="modal fade" id="videoModalpop" tabindex="-1" role="dialog" aria-labelledby="causeAperaModal" aria-hidden="true">
 			  <div class="modal-dialog" role="document">
 			    <div class="modal-content">
 			      <div class="modal-body">
