b04096961f0e8486855be34ab8bd8179d7965363
MrLister <m_r_lister@yahoo.com>
2019-04-15 17:39:09 -0700

    fixed variant

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -208,7 +208,6 @@ add_action( 'woocommerce_after_main_content', 'apera_bags_woocommerce_wrapper_af
 	 */
 	function apera_bags_woocommerce_cart_link_fragment( $fragments ) {
 		ob_start();
-		// apera_bags_woocommerce_cart_link();
 		$fragments['span.cart-contents-count.wonka-badge.badge'] = '<span class="cart-contents-count wonka-badge badge">' . WC()->cart->get_cart_contents_count() . '</span>';
 		ob_get_clean();
 		return $fragments;
@@ -216,30 +215,6 @@ add_action( 'woocommerce_after_main_content', 'apera_bags_woocommerce_wrapper_af
 }
 
 add_filter( 'woocommerce_add_to_cart_fragments', 'apera_bags_woocommerce_cart_link_fragment' );
-
-if ( ! function_exists( 'apera_bags_woocommerce_cart_link' ) ) {
-	/**
-	 * Cart Link.
-	 *
-	 * Displayed a link to the cart including the number of items present and the cart total.
-	 *
-	 * @return void
-	 */
-	function apera_bags_woocommerce_cart_link() {
-		?>
-		<a class="cart-contents" href="<?php echo esc_url( wc_get_cart_url() ); ?>" title="<?php esc_attr_e( 'View your shopping cart', 'apera-bags' ); ?>">
-			<?php
-			$item_count_text = sprintf(
-				/* translators: number of items in the mini cart. */
-				_n( '%d item', '%d items', WC()->cart->get_cart_contents_count(), 'apera-bags' ),
-				WC()->cart->get_cart_contents_count()
-			);
-			?>
-			<span class="amount"><?php echo wp_kses_data( WC()->cart->get_cart_subtotal() ); ?></span> <span class="count"><?php echo esc_html( $item_count_text ); ?></span>
-		</a>
-		<?php
-	}
-}
 
 function wonka_woocommerce_update_order_review_fragments( $fragments ) {
 	ob_start();
