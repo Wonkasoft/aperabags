56394a4095be22820ef2b81583ac9e02ee9919a8
MrLister <m_r_lister@yahoo.com>
2019-04-22 09:55:36 -0700

    css and search page

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,13 +10,13 @@
 ?>
 <div class="col-6 col-md-4">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<header class="entry-header wonka-search-header">
 		<?php apera_bags_post_thumbnail(); ?>
 	</header><!-- .entry-header -->
 	<content class="row wonka-row">
 		<div class="col-12">
 		
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title( sprintf( '<h2 class="entry-title wonka-search-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -28,13 +28,13 @@
 		<?php endif; ?>
 		</div>
 		<div class="col-12">
-		<div class="entry-summary">
+		<div class="entry-summary wonka-search-excerpt">
 			<?php the_excerpt(); ?>
 		</div><!-- .entry-summary -->
 
 		</div>
 	</content>
-	<footer class="entry-footer">
+	<footer class="entry-footer wonka-search-footer">
 		<a class="btn wonka-btn" href="<?php echo get_permalink(); ?>" rel="bookmark">See Details</a>
 		<!-- commented out comments area --> 
 		<!-- <?php apera_bags_entry_footer(); ?> -->
