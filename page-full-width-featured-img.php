5a94fd1a8bca8b8a2f488d21ffa3d3d473219317
MrLister <m_r_lister@yahoo.com>
2019-05-03 23:56:37 -0700

    fixing page template

---
+++ page-full-width-featured-img.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Full Width Featured Image
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package aperabags
+ */
+
+global $post;
+$ws_post_type = ( !empty( $post->post_type ) ) ? ' main-' . $post->post_type: '';
+$ws_post_slug = ( !empty( $post->post_name ) ) ? ' main-' . $post->post_name: '';
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="full-width site-main<?php echo esc_attr( $ws_post_slug . $ws_post_type ); ?>">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page-full-image' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ template-parts/content-page-full-image.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package aperabags
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+	<section class="post-thumbnail-section container-fluid">
+		<div class="row">
+			<div class="col">
+				<?php apera_bags_post_thumbnail(); ?>
+			</div>
+		</div>
+	</section>
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'apera-bags' ),
+			'after'  => '</div>',
+		) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'apera-bags' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-<?php the_ID(); ?> -->
