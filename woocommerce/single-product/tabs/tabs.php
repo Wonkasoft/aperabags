241e5e81755fe40850ed083c15e646088e914226
MrLister <m_r_lister@yahoo.com>
2019-05-04 10:07:12 -0700

    fixing reviews

--- woocommerce/single-product/tabs/tabs.php
+++ woocommerce/single-product/tabs/tabs.php
@@ -42,7 +42,6 @@ if ( ! empty( $tabs ) ) : ?>
 			<?php else: ?>
 				<section id="section-<?php echo esc_attr( $key ); ?>" class="wonka-section wonka-section-<?php echo esc_attr( $key ); ?>">
 					<div class="wonka-Tabs-panel wonka-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content" id="tab-<?php echo esc_attr( $key ); ?>" aria-data="tab-title-<?php echo esc_attr( $key ); ?>">
-						<?php var_dump($tab); ?>
 						<?php if ( isset( $tab['callback'] ) ) { call_user_func( $tab['callback'], $key, $tab ); } ?>
 					</div>
 				</section>
