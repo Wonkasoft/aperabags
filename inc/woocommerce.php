a9db40b40bbf68e631c8f405f6b7364abbc5dce1
MrLister <m_r_lister@yahoo.com>
2019-05-14 01:54:30 -0700

    fixing checkout

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1575,6 +1575,7 @@ function wonka_woocommerce_review_order_before_submit() {
 				document.getElementById( "billing_city" ).removeEventListener( 'keydown', function() { return; }, true );
 				document.getElementById( "billing_state" ).classList.remove( 'state_select' );
 				document.getElementById( "billing_state" ).removeEventListener( 'change', function() { return; }, true );
+				document.getElementById( "billing_state" ).removeEventListener( 'input', function() { return; }, true );
 				document.getElementById( "billing_state" ).removeEventListener( 'click', function() { return; }, true );
 				document.getElementById( "billing_state" ).removeEventListener( 'keydown', function() { return; }, true );
 				document.getElementById( "billing_postcode" ).classList.remove( 'input-text' );
