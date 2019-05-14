ab9182b879d1f23da3fd316e9df38140751afbf3
MrLister <m_r_lister@yahoo.com>
2019-05-14 01:25:51 -0700

    fixing checkout

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1565,20 +1565,21 @@ function wonka_woocommerce_review_order_before_submit() {
 			if ( document.getElementById( 'bill-to-different-address-checkbox2' ).checked ) 
 			{
 				document.getElementById( "billing_address_1" ).classList.remove( 'input-text' );
-				document.getElementById( "billing_address_1" ).removeEventListener( 'change', function() { return; } );
-				document.getElementById( "billing_address_1" ).removeEventListener( 'keydown', function() { return; } );
+				document.getElementById( "billing_address_1" ).removeEventListener( 'change', function() { return; }, true );
+				document.getElementById( "billing_address_1" ).removeEventListener( 'keydown', function() { return; }, true );
 				document.getElementById( "billing_address_2" ).classList.remove( 'input-text' );
-				document.getElementById( "billing_address_2" ).removeEventListener( 'change', function() { return; } );
-				document.getElementById( "billing_address_2" ).removeEventListener( 'keydown', function() { return; } );
+				document.getElementById( "billing_address_2" ).removeEventListener( 'change', function() { return; }, true );
+				document.getElementById( "billing_address_2" ).removeEventListener( 'keydown', function() { return; }, true );
 				document.getElementById( "billing_city" ).classList.remove( 'input-text' );
-				document.getElementById( "billing_city" ).removeEventListener( 'change', function() { return; } );
-				document.getElementById( "billing_city" ).removeEventListener( 'keydown', function() { return; } );
+				document.getElementById( "billing_city" ).removeEventListener( 'change', function() { return; }, true );
+				document.getElementById( "billing_city" ).removeEventListener( 'keydown', function() { return; }, true );
 				document.getElementById( "billing_state" ).classList.remove( 'state_select' );
-				document.getElementById( "billing_state" ).removeEventListener( 'change', function() { return; } );
-				document.getElementById( "billing_state" ).removeEventListener( 'keydown', function() { return; } );
+				document.getElementById( "billing_state" ).removeEventListener( 'change', function() { return; }, true );
+				document.getElementById( "billing_state" ).removeEventListener( 'click', function() { return; }, true );
+				document.getElementById( "billing_state" ).removeEventListener( 'keydown', function() { return; }, true );
 				document.getElementById( "billing_postcode" ).classList.remove( 'input-text' );
-				document.getElementById( "billing_postcode" ).removeEventListener( 'change', function() { return; } );
-				document.getElementById( "billing_postcode" ).removeEventListener( 'keydown', function() { return; } );
+				document.getElementById( "billing_postcode" ).removeEventListener( 'change', function() { return; }, true );
+				document.getElementById( "billing_postcode" ).removeEventListener( 'keydown', function() { return; }, true );
 			}
 			else
 			{
