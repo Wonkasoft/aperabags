2dc16695c0dcea9a74e82cd237e09bc12526a57c
MrLister <rlister@wonkasoft.com>
2019-06-18 16:08:33 -0700

    fixing popup

--- js/wonkamizer-js.js
+++ js/wonkamizer-js.js
@@ -1135,7 +1135,7 @@ if ( wonkasoft_request.ga_id !== '' )
 								img_tainers.setAttribute( 'data-variant-color', variant_selected );
 							}
 
-							if ( img_tainers.getAttribute( 'data-variant-color' ) === variant_selected ) <?php else : ?>
+							if ( img_tainers.getAttribute( 'data-variant-color' ) === variant_selected )
 							{
 								img_tainers.classList.add( 'variant-show' );
 								full_imgs_parent.appendChild( img_tainers );
