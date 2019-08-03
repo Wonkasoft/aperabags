ae89491807c0a0ae93515455078db1dcbf1e5fa9
llister15 <llister@wonkasoft.com>
2019-08-03 13:53:49 -0700

    css

--- template-parts/content-page-full-image.php
+++ template-parts/content-page-full-image.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="full-width-entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h1 class="entry-title" style="display:none;">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 	<section class="post-thumbnail-section container-fluid">
 		<div class="row">
