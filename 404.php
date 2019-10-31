93c9abacc69ca20b9ab28fe1320da28e39d5ae9b
llister15 <llister@wonkasoft.com>
2019-10-31 00:51:48 -0700

    updated 404 page

--- 404.php
+++ 404.php
@@ -10,18 +10,20 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div id="primary" class="content-area container 404-background">
+		<main id="main" class="site-main row">
+			<section class="error-404 not-found text-center col">
+				<header class="page-header text-center col">
+					<h1 class="404-title col">404</h1>
+					<h2 class="sub-title col"><?php esc_html_e( 'Four, oh four', 'apera-bags' ); ?></h2>
+					<h3 class="title-message col">Wherefore Art Thou Four?</h3>
 
-			<section class="error-404 not-found text-center">
-				<header class="page-header">
-					<h1>404-page</h1>
-					<h2 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'apera-bags' ); ?></h2>
-					<?php get_search_form(); ?>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'apera-bags' ); ?></p>
+				<div class="page-content fixed-divider-460">
+					<p><?php esc_html_e( 'Like a great workout coming to a grinding halt!', 'apera-bags' ); ?></p>
+					<p><?php esc_html_e( "Unfortunately, this page doesn't seem to exist.", 'apera-bags' ); ?></p>
+					<?php get_search_form(); ?> or... <a href="" class="wonka-btn">Back Home</a>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
