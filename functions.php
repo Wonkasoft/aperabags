0fdad36098c1ed0771ad551fddf725d6da6ef25a
llister15 <llister@wonkasoft.com>
2019-11-17 19:21:35 -0800

    add role to all customers

--- functions.php
+++ functions.php
@@ -419,3 +419,20 @@ function wonkasoft_add_defer_attribute( $tag, $handle ) {
 	return str_replace( ' src', ' defer src', $tag );
 }
 add_filter( 'script_loader_tag', 'wonkasoft_add_defer_attribute', 10, 2 );
+
+
+
+function add_new_roles() {
+	$user_query = new WP_User_Query( array( 'role' => 'Customer' ) );
+	// User Loop
+	if ( ! empty( $user_query->get_results() ) ) {
+		foreach ( $user_query->get_results() as $user ) {
+			$userobj = new WP_User( $user->ID );
+			$userobj->add_role( 'apera_perks_partner', 'Apera Perks Partner' );
+		}
+	} else {
+		echo 'No users found.';
+	}
+}
+
+add_action( 'admin_head', 'add_new_roles' );
