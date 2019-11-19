09da9c3794482690bde1598249c7972c264f9375
MrLister <rlister@wonkasoft.com>
2019-11-18 22:01:00 -0800

    moving express buttons

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -2166,14 +2166,14 @@ function wonkasoft_express_buttons_checkout() {
 		 *
 		 * @since  1.0.0 This will remove the Apple Google Pay buttons from the checkout page for move
 		 */
-		remove_action( 'woocommerce_checkout_before_customer_details', array( new WC_Stripe_Payment_Request(), 'display_payment_request_button_html' ), 1 );
+		remove_action( 'woocommerce_checkout_before_customer_details', array( 'WC_Stripe_Payment_Request', 'display_payment_request_button_html' ), 1 );
 
 		/**
 		 * This will remove the payment button from the checkout page for move
 		 *
 		 * @since  1.0.0 Remove Stripe buttons on the checkout page for move
 		 */
-		remove_action( 'woocommerce_checkout_before_customer_details', array( new WC_Stripe_Payment_Request(), 'display_payment_request_button_separator_html' ), 2 );
+		remove_action( 'woocommerce_checkout_before_customer_details', array( 'WC_Stripe_Payment_Request', 'display_payment_request_button_separator_html' ), 2 );
 
 		add_action( 'wonka_checkout_express_btns', array( new WC_Stripe_Payment_Request(), 'display_payment_request_button_html' ), 1 );
 		add_action( 'wonka_checkout_express_btns', array( new WC_Stripe_Payment_Request(), 'display_payment_request_button_separator_html' ), 2 );
