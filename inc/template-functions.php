1c5a164f11a2ee6efeb41babef40e393ab3ff1d8
MrLister <rlister@wonkasoft.com>
2020-01-09 16:10:07 -0800

    fixing user profile

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1943,13 +1943,14 @@ function wonkasoft_api_responses_user_data( $user ) {
 		}
 		?>
 	<hr />
-		<div class="header-container"><h3 class="h3 header-text"><?php esc_html_e( 'Apera Affiliate and Contact Info', 'aperabags' ); ?></h3></div>
-
+		<div class="header-container"><h3 class="h3 header-text"><?php esc_html_e( 'Apera Affiliate and Contact Info', 'aperabags' ); ?></h3>
 		<?php
 			echo "<pre>\n";
 			print_r( get_user_meta( $user_id ) );
 			echo "</pre>\n";
 		?>
+		</div>
+
 		<table class="form-table">
 			<tbody>
 					<?php
