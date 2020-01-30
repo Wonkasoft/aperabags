ed1adaddcb3fdea20b13df9cf9876b4d1d6951e4
MrLister <rlister@wonkasoft.com>
2020-01-29 23:40:48 -0800

    fixing date picker for birthday

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1960,9 +1960,6 @@ function wonkasoft_api_responses_user_data( $user ) {
 		$getresponse     = ( ! empty( get_user_meta( $user_id, 'getResponse_data', true ) ) ) ? get_user_meta( $user_id, 'getResponse_data', true ) : '';
 		$company_logo    = ( ! empty( get_user_meta( $user_id, 'company_logo', true ) ) ) ? get_user_meta( $user_id, 'company_logo', true ) : '';
 		$user_birthday   = ( ! empty( get_user_meta( $user_id, 'users_birthday', true ) ) ) ? get_user_meta( $user_id, 'users_birthday', true ) : '';
-		echo "<pre>\n";
-		print_r( $user_birthday );
-		echo "</pre>\n";
 
 		if ( ! empty( $company_logo ) ) {
 			$company_logo = json_decode( $company_logo );
