b5c220cee7b9cab8142f08dc4f1f40efd2ff00de
MrLister <rlister@wonkasoft.com>
2020-03-30 04:33:05 -0700

    fixed description filter

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1273,7 +1273,7 @@ function wonka_filter_woocommerce_short_description( $post_post_excerpt ) {
 		endif;
 		$add_links = '<a id="key-features-link" href="#">Key Features</a> | <a id="product-specs-link" href="#">Product Specs</a> | <a id="review-link" href="#">Reviews</a>' . $compare_link_set;
 
-		if ( ! is_page( 'shop' ) ) :
+		if ( ! is_shop() ) :
 			$post_post_excerpt = $post_post_excerpt . $add_links . ob_get_clean();
 		else :
 			$post_post_excerpt = $post_post_excerpt . ob_get_clean();
