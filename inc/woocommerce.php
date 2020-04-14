4a7fcdc13aa697f156f166ee85800f71635f6741
llister15 <llister@wonkasoft.com>
2020-04-13 22:35:16 -0700

    fixed Product

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -827,6 +827,7 @@ function wonka_checkout_after_checkout_form_custom( $checkout ) {
 				foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 					$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 					if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+						$product_permalink = apply_filters('woocommerce_cart_item_permalink', $_product->is_visible() ? $_product->get_permalink($cart_item) : '', $cart_item, $cart_item_key);
 						?>
 						<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 							<td class="product-thumbnail">
