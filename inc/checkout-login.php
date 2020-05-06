560401981a90df24993efbc5f44124e8c1abb6cd
MrLister <rlister@wonkasoft.com>
2020-05-06 02:41:15 -0700

    checking

--- inc/checkout-login.php
+++ inc/checkout-login.php
@@ -92,13 +92,14 @@ function ws_redirect_pre_checkout() {
 
 	$guest            = ( isset( $_GET['guestcheckout'] ) ) ? wp_kses_data( wp_unslash( $_GET['guestcheckout'] ) ) : false;
 	$redirect_page_id = 16367; // Update this to the page you would like to load before checkout.
-
-	if ( is_user_logged_in() && is_page( $redirect_page_id ) || 'true' === $guest ) {
-		wp_safe_redirect( get_permalink( wc_get_page_id( 'checkout' ) ) );
-		die;
-	} else {
-		wp_safe_redirect( get_permalink( $redirect_page_id ) );
-		die;
-	}
+	if ( is_checkout() ) :
+		if ( is_user_logged_in() && is_page( $redirect_page_id ) || 'true' === $guest ) {
+			wp_safe_redirect( get_permalink( wc_get_page_id( 'checkout' ) ) );
+			die;
+		} else {
+			wp_safe_redirect( get_permalink( $redirect_page_id ) );
+			die;
+		}
+	endif;
 }
 add_action( 'template_redirect', 'ws_redirect_pre_checkout' );
