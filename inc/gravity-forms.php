d7ec2cdce80da84bf77d6bd864e8373b150423c0
llister15 <llister@wonkasoft.com>
2020-05-17 02:29:40 -0700

    update

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -18,18 +18,18 @@ function pre_submission_handler( $form ) {
 	if ( 'Apera Perks Registration Checkout' == $form['title'] ) {
 		?>
 	<script>
-			console.log(jQuery('#shipping_first_name').val());
-			console.log(jQuery('#shipping_last_name').val());
-			console.log(jQuery('#shipping_company').val());
-			console.log(jQuery('#shipping_address_1').val());
-			console.log(jQuery('#shipping_address_2').val());
-			console.log(jQuery('#shipping_city').val());
-			console.log(jQuery('#select2').val());-shipping_state-container
-			console.log(jQuery('#shipping_postcode').val());
-			console.log(jQuery('#shipping_phone').val());
-			console.log(jQuery('#shipping_email').val());
-			console.log(jQuery("input['name']['mc4wp-subscribe']").val());
-			console.log(jQuery('#order_comments').val());
+			console.log("shipping_first_name: "+jQuery('#shipping_first_name').val());
+			console.log("shipping_last_name: " + jQuery('#shipping_last_name').val());
+			console.log("shipping_company: " + jQuery('#shipping_company').val());
+			console.log("shipping_address_1: " + jQuery('#shipping_address_1').val());
+			console.log("shipping_address_2: " + jQuery('#shipping_address_2').val());
+			console.log("shipping_city: " + jQuery('#shipping_city').val());
+			console.log("select2-shipping_state-container: " + jQuery('#select2-shipping_state-container').val());
+			console.log("shipping_postcode: " + jQuery('#shipping_postcode').val());
+			console.log("shipping_phone: " + jQuery('#shipping_phone').val());
+			console.log("shipping_email: " + jQuery('#shipping_email').val());
+			console.log("mc4wp-subscribe: " + jQuery("input[name='mc4wp-subscribe']").val());
+			console.log("order_comments: " + jQuery('#order_comments').val());
 	</script>
 		<?php
 	}
