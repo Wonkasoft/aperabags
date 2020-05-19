7d57ad5aef68234c5c05200ea986919fc2d5b2a1
llister15 <llister@wonkasoft.com>
2020-05-18 23:15:22 -0700

    added url to pre submit gforms

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -30,7 +30,24 @@ function pre_submission_handler( $form ) {
 			console.log("shipping_email: " + jQuery('#shipping_email').val());
 			console.log("mc4wp-subscribe: " + jQuery("input[name='mc4wp-subscribe']").val());
 			console.log("order_comments: " + jQuery('#order_comments').val());
-	</script>
+
+
+			var url = window.location.href;
+			var shipping_first_name = jQuery('#shipping_first_name').val());
+			var shipping_last_name =  jQuery('#shipping_last_name').val());
+			var shipping_company =  jQuery('#shipping_company').val());
+			var shipping_address_1 =  jQuery('#shipping_address_1').val());
+			var shipping_address_2 =  jQuery('#shipping_address_2').val());
+			var shipping_city =  jQuery('#shipping_city').val());
+			var shipping_state =  jQuery('#shipping_state').val());
+			var shipping_postcode =  jQuery('#shipping_postcode').val());
+			var shipping_phone =  jQuery('#shipping_phone').val());
+			var shipping_email =  jQuery('#shipping_email').val());
+			var mc4wp-subscribe =  jQuery("input[name='mc4wp-subscribe']").val());
+			var order_comments =  jQuery('#order_comments').val());
+			console.log(url);
+			window.location.href = url + "&shipping_first_name=" + shipping_first_name + "&shipping_last_name=" + shipping_last_name + "&shipping_company=" + shipping_company + "&shipping_address_1=" + "&shipping_address_2=" + shipping_address_2 + "&shipping_city=" + shipping_city + "&shipping_state=" + shipping_state + "&shipping_postcode=" + shipping_postcode + "&shipping_phone=" + shipping_phone + "&shipping_email=" + shipping_email + "&mc4wp-subscribe=" + mc4wp-subscribe + "&order_comments" + order_comments;
+	 </script>
 		<?php
 	}
 }
