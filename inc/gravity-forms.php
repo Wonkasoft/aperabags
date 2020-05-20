8458a490090cb0386f00406f59a329e00f417486
llister15 <llister@wonkasoft.com>
2020-05-20 01:31:13 -0700

    update

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -16,44 +16,36 @@
  */
 function pre_submission_handler( $form ) {
 	if ( 'Apera Perks Registration Checkout' == $form['title'] ) {
-		?>
-	<script>
-			console.log("shipping_first_name: "+ jQuery('#shipping_first_name').val());
-			console.log("shipping_last_name: " + jQuery('#shipping_last_name').val());
-			console.log("shipping_company: " + jQuery('#shipping_company').val());
-			console.log("shipping_address_1: " + jQuery('#shipping_address_1').val());
-			console.log("shipping_address_2: " + jQuery('#shipping_address_2').val());
-			console.log("shipping_city: " + jQuery('#shipping_city').val());
-			console.log("shipping_state: " + jQuery('#shipping_state').val());
-			console.log("shipping_postcode: " + jQuery('#shipping_postcode').val());
-			console.log("shipping_phone: " + jQuery('#shipping_phone').val());
-			console.log("shipping_email: " + jQuery('#shipping_email').val());
-			console.log("mc4wp-subscribe: " + jQuery("input[name='mc4wp-subscribe']").val());
-			console.log("order_comments: " + jQuery('#order_comments').val());
 
+		echo "<script>
+			console.log('shipping_first_name: '+ jQuery('#shipping_first_name').val());
+			console.log('shipping_last_name: ' + jQuery('#shipping_last_name').val());
+			console.log('shipping_company: ' + jQuery('#shipping_company').val());
+			console.log('shipping_address_1: ' + jQuery('#shipping_address_1').val());
+			console.log('shipping_address_2: ' + jQuery('#shipping_address_2').val());
+			console.log('shipping_city: ' + jQuery('#shipping_city').val());
+			console.log('shipping_state: ' + jQuery('#shipping_state').val());
+			console.log('shipping_postcode: ' + jQuery('#shipping_postcode').val());
+			console.log('shipping_phone: ' + jQuery('#shipping_phone').val());
+			console.log('shipping_email: ' + jQuery('#shipping_email').val());
+			console.log('mc4wp-subscribe: ' + jQuery('input[name='mc4wp-subscribe']').val());
+			console.log('order_comments: ' + jQuery('#order_comments').val());
 
-			var url = window.location.href;
-			var shipping_first_name = jQuery('#shipping_first_name').val();
-			<?php $first_name =; ?>jQuery('#shipping_first_name').val(); <?php ; ?>
-			var shipping_last_name =  jQuery('#shipping_last_name').val();
-			var shipping_company =  jQuery('#shipping_company').val();
-			var shipping_address_1 =  jQuery('#shipping_address_1').val();
-			var shipping_address_2 =  jQuery('#shipping_address_2').val();
-			var shipping_city =  jQuery('#shipping_city').val();
-			var shipping_state =  jQuery('#shipping_state').val();
-			var shipping_postcode =  jQuery('#shipping_postcode').val();
-			var shipping_phone =  jQuery('#shipping_phone').val();
-			var shipping_email =  jQuery('#shipping_email').val();
-			var mc4wp_subscribe =  jQuery("input[name='mc4wp-subscribe']").val();
-			var order_comments =  jQuery('#order_comments').val();
-			console.log(url);
-			url = encodeURI(url + "&shipping_first_name=" + shipping_first_name + "&shipping_last_name=" + shipping_last_name + "&shipping_company=" + shipping_company + "&shipping_address_1=" + "&shipping_address_2=" + shipping_address_2 + "&shipping_city=" + shipping_city + "&shipping_state=" + shipping_state + "&shipping_postcode=" + shipping_postcode + "&shipping_phone=" + shipping_phone + "&shipping_email=" + shipping_email + "&mc4wp_subscribe=" + mc4wp_subscribe + "&order_comments=" + order_comments);
-			window.location.href = url;
-			console.log(url);
-	 </script>
-		<?php
+			$shipping_first_name = jQuery('#shipping_first_name').val();
+			$shipping_last_name =  jQuery('#shipping_last_name').val();
+			$shipping_company =  jQuery('#shipping_company').val();
+			$shipping_address_1 =  jQuery('#shipping_address_1').val();
+			$shipping_address_2 =  jQuery('#shipping_address_2').val();
+			$shipping_city =  jQuery('#shipping_city').val();
+			$shipping_state =  jQuery('#shipping_state').val();
+			$shipping_postcode =  jQuery('#shipping_postcode').val();
+			$shipping_phone =  jQuery('#shipping_phone').val();
+			$shipping_email =  jQuery('#shipping_email').val();
+			$mc4wp_subscribe =  jQuery('input[name='mc4wp-subscribe']').val();
+			$order_comments =  jQuery('#order_comments').val();
+	 </script>";
 		echo '<pre>';
-		print_r( $first_name );
+		print_r( $shipping_first_name );
 		echo '</pre>';
 	}
 }
