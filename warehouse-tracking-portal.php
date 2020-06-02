d21bbd174f64a579357612c33bc5c0f220c59d5f
MrLister <rlister@wonkasoft.com>
2020-06-02 01:11:18 -0700

    fixed template

--- warehouse-tracking-portal.php
+++ warehouse-tracking-portal.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template name: Warehouse Tracking portal
+ * Template name: Warehouse Tracking Portal
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
