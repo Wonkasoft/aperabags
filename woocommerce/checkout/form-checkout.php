fc67b0d5242b126608ebada96827f7d0c4853583
MrLister <rlister@wonkasoft.com>
2020-06-17 03:45:52 -0700

    fixed default shipping

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -18,7 +18,6 @@
 defined( 'ABSPATH' ) || exit;
 
 $guest = ( isset( $_GET['guestcheckout'] ) ) ? wp_kses_post( wp_unslash( $_GET['guestcheckout'] ) ) : 'false';
-WC()->session->set( 'chosen_shipping_methods', array() );
 
 if ( ! is_user_logged_in() && 'false' === $guest ) :
 	echo do_shortcode( '[wc_login_register]' );
