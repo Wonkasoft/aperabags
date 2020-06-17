90e529712fec55fc59f690fcbb81972812572ba6
MrLister <rlister@wonkasoft.com>
2020-06-17 14:38:01 -0700

    fixed checkout

--- js/wonkamizer-js.js
+++ js/wonkamizer-js.js
@@ -3603,7 +3603,6 @@ var componentForm;
 
 
 		if ( document.querySelector( 'body.woocommerce-checkout' ) ) {
-			document.querySelector( '#wc-stripe-cc-form label[for="card-element"]' ).style = 'display: none;';
 			document.body.addEventListener( 'keydown', function( e ) {
 				if ( 13 === e.keyCode ) {
 					e.stopImmediatePropagation();
