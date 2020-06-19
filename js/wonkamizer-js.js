b83217663314b50bd9e4cecbb29f70292ca9d13f
MrLister <rlister@wonkasoft.com>
2020-06-19 02:45:40 -0700

    updated JS

--- js/wonkamizer-js.js
+++ js/wonkamizer-js.js
@@ -4505,15 +4505,9 @@ var componentForm;
 		$( document.body ).on( 'select_default_shipping', function( e ) {
 			document.querySelector( '#shipping_method input' ).click();
 		});
-
-		var qty_reset_timer;
 		
 		$( document.body ).on( 'added_to_cart removed_from_cart wc_fragments_refreshed wc_fragments_loaded wc_fragment_refresh updated_wc_div update_checkout updated_checkout updated_cart_totals', function( e ) { 
 			if ( document.querySelector( 'body.woocommerce-checkout' ) ) {
-				if ( '' != qty_reset_timer ) {
-					clearTimeout( qty_reset_timer );
-				}
-				
 				checkout_init.qty_changers_init();
 			}
 		});
